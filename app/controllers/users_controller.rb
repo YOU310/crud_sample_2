692917fe9e16f2b56f31585b61ceea710d3e7e39
YOU310 <75014820+YOU310@users.noreply.github.com>
2021-11-12 12:00:00 +0900

    コントローラにupdateアクション追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,7 +15,12 @@ class UsersController < ApplicationController
 
   def edit
     @user = User.find(params[:id])
-  end  
+  end
+
+  def update
+    user = User.find(params[:id])
+    user.update(user_params)
+  end
 
   def destroy
     user = User.find(params[:id])
